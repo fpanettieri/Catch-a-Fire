1ba4b9fd1deb166e1be1cae07985e376bfccae42
Fabio R. Panettieri <fpanettieri@gmail.com>
2013-07-30 18:24:21 -0300

    Working hands

--- Assets/Scripts/Hands.cs
+++ Assets/Scripts/Hands.cs
@@ -13,35 +13,44 @@ public class Hands : MonoBehaviour
 	private const int OBJECTS_MASK = 1 << 10 | 1 << 9;
 	private const int ROOM_MASK = 1 << 8;
 	
-	public void FixedUpdate ()
+	public void Update ()
 	{
-		if(Input.GetMouseButton(0)){
+		if(Input.GetMouseButtonDown(0)){
 			m_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			ProcessTouch(m_ray, 0);
+			ProcessTouch(m_ray, 0, TouchPhase.Began);
+			
+		} else if(Input.GetMouseButtonUp(0)){
+			m_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			ProcessTouch(m_ray, 0, TouchPhase.Ended);
+			
+		} else if(Input.GetMouseButton(0)){
+			m_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			ProcessTouch(m_ray, 0, TouchPhase.Moved);
 		}
 		
 		foreach(Touch touch in Input.touches) {
 			m_ray = Camera.main.ScreenPointToRay(touch.position);
-			ProcessTouch(m_ray, touch.fingerId);
+			ProcessTouch(m_ray, touch.fingerId, touch.phase);
 		}
 	}
 	
-	private void ProcessTouch(Ray _ray, int _fingerId)
+	private void ProcessTouch(Ray _ray, int _fingerId, TouchPhase _phase)
 	{
-		if( ShouldDrag(_ray, _fingerId) ){
+		if( ShouldDrag(_ray, _fingerId, _phase) ){
 			Drag(_ray, _fingerId);
 			
-		} else if( ShouldGrab(_ray, _fingerId) ){
+		} else if( ShouldGrab(_ray, _fingerId, _phase) ){
 			Grab(m_draggable, _fingerId);
 		
-		} else if( ShouldPush(_ray, _fingerId) ){
+		} else if( ShouldPush(_ray, _fingerId, _phase) ){
 			Push(_ray, _fingerId);
 		}
 	}
 
-	private bool ShouldDrag(Ray _ray, int _fingerId)
+	private bool ShouldDrag(Ray _ray, int _fingerId, TouchPhase _phase)
 	{
 		if( _fingerId >= m_draggers.Length ){ return false; }
+		if( _phase == TouchPhase.Canceled || _phase == TouchPhase.Ended ){ return false; }
 		
 		Dragger dragger = m_draggers[_fingerId].GetComponent<Dragger>();
 		if( dragger.dragging ){ return true; }
@@ -49,18 +58,10 @@ public class Hands : MonoBehaviour
 		return false;
 	}
 
-	private void Drag(Ray _ray, int _fingerId)
-	{
-		Physics.Raycast(m_ray, out m_hit, 100, ROOM_MASK);
-		Debug.DrawLine (m_ray.origin, m_hit.point);
-		
-		Dragger dragger = m_draggers[_fingerId].GetComponent<Dragger>();
-		dragger.Drag(m_hit.point);
-	}
-
-	private bool ShouldGrab(Ray _ray, int _fingerId)
+	private bool ShouldGrab(Ray _ray, int _fingerId, TouchPhase _phase)
 	{
 		if( _fingerId >= m_draggers.Length ){ return false; }
+		if( _phase != TouchPhase.Began ){ return false; }
 		
 		Physics.Raycast(m_ray, out m_hit, 100, OBJECTS_MASK);
 		Debug.DrawLine (m_ray.origin, m_hit.point);
@@ -71,17 +72,27 @@ public class Hands : MonoBehaviour
 		return m_draggable != null;
 	}
 	
+	private bool ShouldPush(Ray _ray, int _fingerId, TouchPhase _phase)
+	{
+		if( _fingerId >= m_fingers.Length ){ return false; }
+		return true;
+	}
+	
+	private void Drag(Ray _ray, int _fingerId)
+	{
+		Physics.Raycast(m_ray, out m_hit, 100, ROOM_MASK);
+		Debug.DrawLine (m_ray.origin, m_hit.point);
+		
+		Dragger dragger = m_draggers[_fingerId].GetComponent<Dragger>();
+		dragger.Drag(m_hit.point);
+	}
+	
 	private void Grab(Draggable _draggable, int _fingerId)
 	{
 		Dragger dragger = m_draggers[_fingerId].GetComponent<Dragger>();
 		dragger.Grab(_draggable, m_hit.point);
 	}
 
-	private bool ShouldPush(Ray _ray, int _fingerId)
-	{
-		if( _fingerId >= m_fingers.Length ){ return false; }
-		return true;
-	}
 	
 	private void Push(Ray _ray, int _fingerId)
 	{
