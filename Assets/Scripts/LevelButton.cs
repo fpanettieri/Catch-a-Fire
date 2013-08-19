9c1e9c08ad69be68f0f1ea1e1d64dedcc5c775c7
Fabio R. Panettieri <fpanettieri@gmail.com>
2013-08-19 12:22:19 -0300

    Level buttons

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButton : MonoBehaviour
+{
+	// injected dependencies
+	public UISprite m_lock;
+	
+	// configuration
+	public bool m_forceUnlock;
+	public string m_levelName;
+	
+	public void Start()
+	{
+		if(Unlocked()){
+			UnlockLevel();
+		} else {
+			LockLevel();
+		}
+	}
+	
+	private bool Unlocked()
+	{
+		if( m_forceUnlock ){ return true; }
+		if( PlayerPrefs.HasKey( m_levelName + "Unlocked" ) ){ return true; }
+		return false;
+	}
+	
+	private void LockLevel()
+	{
+		m_lock.enabled = true;
+		collider.enabled = false;
+	}
+	
+	private void UnlockLevel()
+	{
+		m_lock.enabled = false;
+		collider.enabled = true;
+	}
+	
+	public void OnClick ()
+	{
+		if (string.IsNullOrEmpty(m_levelName)){ return; }
+		Application.LoadLevel(m_levelName);
+	}
+}
