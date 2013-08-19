2fad9aa48ec8e5728b3de84e08c905000d7feb9f
Fabio R. Panettieri <fpanettieri@gmail.com>
2013-08-18 21:19:05 -0300

    Target + basic score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+	// injected dependency
+	public UILabel m_label;
+	
+	// configuration
+	public int m_extinguisher;
+	public int m_timer;
+	
+	// internal state
+	private int m_score;
+	private int m_displayed;
+	
+	public void Start ()
+	{
+		m_score = 0;
+		m_displayed = 0;
+	}
+	
+	public void Update ()
+	{
+		if( m_score == m_displayed ) {
+			return;
+
+		} else {
+			m_displayed = m_score;
+			m_label.text = Mathf.RoundToInt( m_displayed ).ToString();
+		}
+	}
+	
+	public void AddScore(int score)
+	{
+		m_score += score;
+	}
+	
+	public void SubstractScore(int score)
+	{
+		m_score -= score;
+	}
+	
+	public void BonusScore(int score)
+	{
+		m_score += score;
+	}
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,23 +3,43 @@ using System.Collections;
 
 public class Target : MonoBehaviour
 {
+	// injected dependencies
 	public GameObject m_circle;
 	public GameObject m_center;
+	public Score m_score;
 	
+	// state accessor for objectives
 	public TargetState state { get { return m_state; } }
 	
-	private TargetState m_state;
+	// internal state
 	private Extinguisher m_extinguisher;
+	private TargetState m_state;
+	private int m_calculatedScore;
 	
 	public void Start()
 	{
 		m_state = TargetState.Empty;
+		m_calculatedScore = 0;
 	}
 	
 	public void FixedUpdate()
 	{
 		if (m_extinguisher == null){ return; }
 		
+		if (m_extinguisher.rigidbody.IsSleeping()) {
+			
+			if(m_extinguisher.transform.up.y >= .9){
+				changeState( TargetState.Good );
+				// TODO: play success sound
+				
+			} else {
+				changeState( TargetState.Bad );
+				// TODO: play error sound
+			}
+			
+		} else {
+			changeState( TargetState.Hover );
+		}
 	}
 	
 	public void OnTriggerEnter(Collider other)
@@ -31,8 +51,7 @@ public class Target : MonoBehaviour
 		if(extinguisher == null){ return; }
 		
 		m_extinguisher = extinguisher;
-		m_state = TargetState.Hover;
-		UpdateColor();
+		changeState( TargetState.Hover );
 	}
 	
 	public void OnTriggerExit(Collider other)
@@ -44,19 +63,49 @@ public class Target : MonoBehaviour
 		if(extinguisher != m_extinguisher){ return; }
 		
 		m_extinguisher = null;
-		m_state = TargetState.Empty;
-		UpdateColor();
+		changeState( TargetState.Empty );
 	}
 	
-	private void UpdateColor()
+	private void changeState(TargetState _state)
+	{
+		if(m_state == _state){ return; }
+		
+		if(m_state == TargetState.Good){ 
+			m_score.SubstractScore(m_calculatedScore);
+		}
+		
+		if(_state == TargetState.Good){
+			AddScore();
+		}
+		
+		m_state = _state;
+		UpdateColor(_state);
+	}
+	
+	private void AddScore()
+	{
+		float distance = Vector3.Distance(m_extinguisher.transform.position, transform.position );
+		
+		if( distance < 0.125f ){
+			m_calculatedScore = m_score.m_extinguisher;
+		} else {
+			float multiplier = Mathf.Clamp( 1 - distance, 0.1f, 1f);
+			m_calculatedScore = Mathf.RoundToInt( multiplier * m_score.m_extinguisher );
+		}
+
+		m_score.AddScore(m_calculatedScore);
+	}
+	
+	private void UpdateColor( TargetState _state )
 	{
 		Color color = Color.magenta;
-		switch(m_state){
+		switch(_state){
 			case TargetState.Empty: color = Color.white;  break;
 			case TargetState.Hover: color = Color.yellow; break;
 			case TargetState.Good:  color = Color.green;  break;
 			case TargetState.Bad:   color = Color.red;    break;
 		}
+		
 		m_circle.renderer.material.color = color;
 		m_center.renderer.material.color = color;
 	}
