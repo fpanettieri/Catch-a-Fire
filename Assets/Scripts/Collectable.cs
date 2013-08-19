efdf56383c9211460f38deadee10a2d1901798d2
Fabio R. Panettieri <fpanettieri@gmail.com>
2013-08-19 11:12:41 -0300

    LevelSelect Screen

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour
+{
+
+	public void Start ()
+	{
+		transform.LookAt(Camera.main.transform);
+		transform.Rotate(0, 180, 0);
+	}
+}
